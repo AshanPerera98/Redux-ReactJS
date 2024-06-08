c4a30ac640fa910fd6c52a0a52bbbafbaacccfd9
Ashan Perera <ashantiwankaperera@gmail.com>
2024-06-08 23:10:27 +0530

    increment action and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./store/store";
+import { increment } from "./store/counter/counterSlice";
 
 function App() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <>
       <h1>Redux Toolkit</h1>
+      <h3>{count}</h3>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -11,7 +11,13 @@ const initialState: CounterState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
 });
+
+export const { increment } = counterSlice.actions;
 
 export default counterSlice.reducer;
